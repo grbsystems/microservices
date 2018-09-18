a62670788d0c9b5e72d85090467237e773358548
Jeremy Burton <jeremy.g.burton@leidos.com>
2018-09-18 16:40:04 -0400

    Added part 4 with mods for sprint boot 2.0

--- counterservice/src/main/java/com/leidos/springboot/counterservice/api/CounterController.java
+++ counterservice/src/main/java/com/leidos/springboot/counterservice/api/CounterController.java
@@ -1,17 +1,27 @@
 package com.leidos.springboot.counterservice.api;
 
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@Configuration
+@EnableAutoConfiguration
+@RefreshScope
 public class CounterController {
 
     private static AtomicLong count = new AtomicLong(0);
 
+    @Value(value = "${counter.prefixMessage}")
+    private String prefixMessage;
+
     @GetMapping(path = "/count")
-    public long getCount(){
-        return count.getAndIncrement();
+    public String getCount(){
+        return prefixMessage + count.getAndIncrement();
     }
 }
