8234fbe1503b1bca219a2209f71e6fce66585cc1
Jeremy Burton <jeremy.g.burton@leidos.com>
2018-09-19 14:31:21 -0400

    Part five complete

--- counterservice/src/main/java/com/leidos/springboot/counterservice/api/CounterController.java
+++ counterservice/src/main/java/com/leidos/springboot/counterservice/api/CounterController.java
@@ -3,6 +3,7 @@ package com.leidos.springboot.counterservice.api;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import java.util.concurrent.atomic.AtomicLong;
 @Configuration
 @EnableAutoConfiguration
 @RefreshScope
+@EnableDiscoveryClient
 public class CounterController {
 
     private static AtomicLong count = new AtomicLong(0);
