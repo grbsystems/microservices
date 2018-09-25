696fb8d3c1108e579952e14162a1fbdaab89f133
Jeremy Burton <jeremy.g.burton@leidos.com>
2018-09-25 16:00:49 -0400

    Tweaks for testing tests

--- counterservice/src/test/java/com/leidos/springboot/counterservice/CounterserviceApplicationTests.java
+++ counterservice/src/test/java/com/leidos/springboot/counterservice/CounterserviceApplicationTests.java
@@ -1,16 +1,20 @@
 package com.leidos.springboot.counterservice;
 
+import com.leidos.springboot.counterservice.api.CounterController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+@WebMvcTest(CounterController.class)
 public class CounterserviceApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assert(false);
 	}
 
 }
